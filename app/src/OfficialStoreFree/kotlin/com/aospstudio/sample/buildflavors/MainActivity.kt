8124aa61ffa56564c2389ebdb7b8eae8c12a7c36
Onur Çil <47140529+onurrcil@users.noreply.github.com>
2021-02-21 16:40:43 +0300

    First version

---
+++ app/src/OfficialStoreFree/kotlin/com/aospstudio/sample/buildflavors/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.aospstudio.sample.buildflavors
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.os.Bundle
+import com.aospstudio.sample.buildflavors.R
+
+class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.text)
+        textView!!.text = "Official Store Free variant"
+    }
+}
